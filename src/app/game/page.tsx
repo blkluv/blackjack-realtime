e28bd063bf0300331498455ec30a65f77985de2e
Sangeet Banerjee <sangeetbanerjee777@gmail.com>
2025-02-11 17:16:33 +0530

    stage 1

---
+++ src/app/game/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { nanoid } from "nanoid";
+// @ts-ignore
+import Card from "@heruka_urgyen/react-playing-cards";
+import { cn, getRandomCard } from "@/lib/utils";
+import Image from "next/image";
+import { useWindowSize } from "../../../hooks/useWindowSize";
+
+const GamePage = () => {
+  return <div>GamePage</div>;
+};
+
+export default GamePage;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,30 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const getRandomCard = (): string => {
+  const rank = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "T",
+    "J",
+    "Q",
+    "K",
+    "A",
+  ];
+  const suit = ["c", "d", "h", "s"];
+
+  const randomRank = rank[Math.floor(Math.random() * rank.length)];
+  const randomSuit = suit[Math.floor(Math.random() * suit.length)];
+
+  return (randomRank ?? "2") + (randomSuit ?? "c");
+};
