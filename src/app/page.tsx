4b3d158fd508b3f0b44a61053b27830d05cd0ca4
Sangeet Banerjee <sangeetbanerjee777@gmail.com>
2025-02-11 11:53:57 +0530

    added demo cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { Button } from '@/components/ui/button';
 import { Card } from './components/Card';
+import { nanoid } from 'nanoid';
 
 export default function Page() {
   return (
@@ -9,43 +10,51 @@ export default function Page() {
         Retro Blackjack
       </h1>
 
-      <div className="max-w-2xl mx-auto">
-        <div className="mb-8">
+      <div className="flex flex-col space-y-16 items-center w-full">
+        {/* dealer */}
+        <div className="flex flex-col w-fit items-center">
           <h2 className="text-xl mb-2">Dealer</h2>
           <div className="flex justify-center">
             <Card value="?" />
             <Card value="10" />
           </div>
         </div>
-
-        <div className="mb-8">
-          <h2 className="text-xl mb-2">Player</h2>
-          <div className="flex justify-center">
-            <Card value="7" />
-            <Card value="K" />
-          </div>
+        {/* players */}
+        <div className="flex space-x-8">
+          {Array.from({ length: 5 }).map((_, i) => (
+            <div
+              key={nanoid()}
+              className="flex flex-col space-y-2 items-center"
+            >
+              <div className="text-sm">Player {i + 1}</div>
+              <div className="flex space-x-8">
+                <Card value="7" />
+                <Card value="K" />
+              </div>
+              <div className="flex space-x-2">
+                <Button
+                  size={'sm'}
+                  variant="outline"
+                  className="bg-yellow-400 text-black hover:bg-yellow-500 cursor-pointer"
+                >
+                  Hit
+                </Button>
+                <Button
+                  size={'sm'}
+                  variant="outline"
+                  className="bg-yellow-400 text-black hover:bg-yellow-500 cursor-pointer"
+                >
+                  Stand
+                </Button>
+              </div>
+            </div>
+          ))}
         </div>
-
-        <div className="text-center mb-4">
-          <p className="text-xl mb-2">Player's Total: 17</p>
-        </div>
-
+        {/* extra buttons */}
         <div className="flex justify-center space-x-4">
           <Button
             variant="outline"
-            className="bg-yellow-400 text-black hover:bg-yellow-500"
-          >
-            Hit
-          </Button>
-          <Button
-            variant="outline"
-            className="bg-yellow-400 text-black hover:bg-yellow-500"
-          >
-            Stand
-          </Button>
-          <Button
-            variant="outline"
-            className="bg-red-600 text-white hover:bg-red-700"
+            className="bg-red-600 text-white hover:bg-red-700 cursor-pointer"
           >
             New Game
           </Button>
